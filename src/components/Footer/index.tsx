d1786aed6a1ae5507a7c3d5a92faa7ccc019b3ea
YaroslavWeb <yaroslavweb98@gmail.com>
2022-08-01 20:30:27 +0800

    update footer link

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,13 +2,12 @@ import { Link } from 'react-router-dom'
 
 import * as S from './styles'
 
-
 export const Footer = () => {
   return (
     <S.Footer>
       © 2021 Developed by
       <Link
-        to={{ pathname: 'https://discord.gg/whbVvgFmk2' }}
+        to={{ pathname: 'https://github.com/YaroslavWeb' }}
         target="_blank"
         style={{ marginLeft: 4 }}
       >
